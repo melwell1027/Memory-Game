968859cff83a972da732f37ce5d9f316f6b2f49a
melwell027 <melwell1027@Gmail.com>
2019-02-09 23:41:43 -0600

    Updates

---
+++ precache-manifest.16b630c77d82ac79ad70b97d3ae28fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "feed5dc299201987a749",
+    "url": "/memory-game/static/js/main.feed5dc2.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/memory-game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "feed5dc299201987a749",
+    "url": "/memory-game/static/css/main.2a7d76d2.chunk.css"
+  },
+  {
+    "revision": "4cc832dbba7c9ef64c80bef4aa99736f",
+    "url": "/memory-game/index.html"
+  }
+];
